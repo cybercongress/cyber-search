068f72645364000e05dbb5a710bda234fce830e4
Hleb Albau <hleb.albau@gmail.com>
2017-09-26 13:47:48 +0300

    #3 increase kafka messages, update connect serialisation

--- core/src/main/kotlin/fund/cyber/node/model/Bitcoin.kt
+++ core/src/main/kotlin/fund/cyber/node/model/Bitcoin.kt
@@ -3,9 +3,9 @@ package fund.cyber.node.model
 import com.datastax.driver.mapping.annotations.Table
 import com.datastax.driver.mapping.annotations.UDT
 import java.math.BigDecimal
-import java.time.Instant
 
 
+interface BitcoinItem
 
 /*
 *
@@ -24,12 +24,14 @@ data class BitcoinBlock(
         val weight: Int,
         val bits: String,
         val difficulty: BigDecimal,
+        val tx_number : Int,
+        val total_outputs_value: String,
         val txs: List<BitcoinBlockTransaction>
-)
+) : BitcoinItem
 
 data class BitcoinBlockTransaction(
         val fee: String,
-        val lock_time: Instant,
+        val lock_time: Long,
         val hash: String,
         val ins: List<BitcoinBlockTransactionIO>,
         val outs: List<BitcoinBlockTransactionIO>
@@ -37,7 +39,7 @@ data class BitcoinBlockTransaction(
 
 data class BitcoinBlockTransactionIO(
         val address: String,
-        val amount: BigDecimal
+        val amount: String
 )
 
 
@@ -52,16 +54,17 @@ data class BitcoinBlockTransactionIO(
 data class BitcoinTransaction(
         val txId: String,
         val block_number: Long,
+        val block_hash : String,
         val coinbase: String? = null,
         val lock_time: Long, //indicate earliest block when that transaction may be added to the block chain //todo make separate field with time
-        val blockTime: String,
+        val block_time: String,
         val size: Int,
         val fee: String,
         val total_input: String,
         val total_output: String,
         val ins: List<BitcoinTransactionIn>,
         val outs: List<BitcoinTransactionOut>
-) {
+) : BitcoinItem {
 
     fun getOutputByNumber(number: Int): BitcoinTransactionOut = outs.find { out -> out.out == number }!!
 }
@@ -75,6 +78,7 @@ data class BitcoinTransactionIn(
         val tx_out: Int
 ) {
 
+    //used by gson to create instance
     constructor() : this("", "0", "", "", 0)
 }
 
@@ -87,5 +91,6 @@ data class BitcoinTransactionOut(
         val required_signatures: Int
 ) {
 
+    //used by gson to create instance
     constructor() : this("", "0", "", 0, 1)
 }
