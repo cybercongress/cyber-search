e8125d8915869061abe30a7e80509cdf89895921
Hleb Albau <hleb.albau@gmail.com>
2017-10-20 20:26:18 +0300

    #18 Add addresses as stream items for ethereum processor

--- stream-processing/ethereum/src/main/kotlin/fund/cyber/index/ethereum/EthereumBlockSplitterApplication.kt
+++ stream-processing/ethereum/src/main/kotlin/fund/cyber/index/ethereum/EthereumBlockSplitterApplication.kt
@@ -1,15 +1,16 @@
 package fund.cyber.index.ethereum
 
+import fund.cyber.dao.ethereum.EthereumDaoService
 import fund.cyber.index.IndexTopics.ethereumSourceTopic
 import fund.cyber.index.ethereum.ApplicationContext.cassandra
 import fund.cyber.index.ethereum.ApplicationContext.streamsConfiguration
+import fund.cyber.index.ethereum.converter.EthereumAddressConverter
 import fund.cyber.index.ethereum.converter.EthereumParityToDaoConverter
-import fund.cyber.node.model.EthereumBlock
-import fund.cyber.node.model.EthereumItem
-import fund.cyber.node.model.EthereumTransaction
+import fund.cyber.node.model.*
 import org.apache.kafka.streams.KafkaStreams
 import org.apache.kafka.streams.kstream.KStreamBuilder
 import org.apache.kafka.streams.kstream.Predicate
+import org.ehcache.Cache
 import org.slf4j.LoggerFactory
 import org.web3j.protocol.core.methods.response.EthBlock
 import java.math.BigInteger
@@ -38,15 +39,21 @@ object EthereumBlockSplitterApplication {
                 .flatMapValues { parityBlock ->
                     log.debug("Processing ${parityBlock.number} block")
                     lastProcessedBlockNumber = parityBlock.number
-                    processBlock(parityBlock)
+                    convertParityBlockToEthereumItems(parityBlock)
                 }
                 .branch(
                         Predicate { _, item -> item is EthereumBlock },
-                        Predicate { _, item -> item is EthereumTransaction }
+                        Predicate { _, item -> item is EthereumTransaction },
+                        Predicate { _, item -> item is EthereumAddress },
+                        Predicate { _, item -> item is EthereumAddressTransaction }
                 )
 
         ethereumItemsStream[0].mapValues { v -> v as EthereumBlock }.to(null, ethereumBlockSerde, "ethereum_block")
         ethereumItemsStream[1].mapValues { v -> v as EthereumTransaction }.to(null, ethereumTxSerde, "ethereum_tx")
+        ethereumItemsStream[2].mapValues { v -> v as EthereumAddress }.to(null, ethereumAddressSerde, "ethereum_address")
+        ethereumItemsStream[3].mapValues { v -> v as EthereumAddressTransaction }
+                .to(null, ethereumAddressTransactionSerde, "ethereum_address_tx")
+
 
         val streams = KafkaStreams(builder, streamsConfiguration.streamProperties())
 
@@ -62,8 +69,10 @@ object EthereumBlockSplitterApplication {
         })
     }
 
-    private fun processBlock(
+    private fun convertParityBlockToEthereumItems(
             parityBlock: EthBlock.Block, tryNumber: Int = 0,
+            addressCache: Cache<String, EthereumAddress> = ApplicationContext.addressCache,
+            addressConverter: EthereumAddressConverter = ApplicationContext.addressConverter,
             converter: EthereumParityToDaoConverter = ApplicationContext.parityToDaoConverter): List<EthereumItem> {
 
         return try {
@@ -71,15 +80,29 @@ object EthereumBlockSplitterApplication {
             val transactions = converter.parityTransactionsToDao(parityBlock)
             val block = converter.parityBlockToDao(parityBlock)
 
-            mutableListOf<EthereumItem>().plus(block).plus(transactions)
+            val existingAddressesUsedInBlock = getExistingAddressesUsedInBlock(block)
+            val updatedAddresses = addressConverter.updateAddressesSummary(block, existingAddressesUsedInBlock)
+            val addressesTransactions = addressConverter.transactionsPreviewsForAddresses(block)
+
+            updatedAddresses.forEach { address -> addressCache.put(address.id, address) }
+
+
+            mutableListOf<EthereumItem>().plus(block).plus(transactions).plus(updatedAddresses).plus(addressesTransactions)
         } catch (e: Exception) {
             if (tryNumber > 100) {
                 log.error("Error during processing block ${parityBlock.number}", e)
                 throw RuntimeException(e)
             } else {
                 Thread.sleep(1000)
-                processBlock(parityBlock, tryNumber + 1)
+                convertParityBlockToEthereumItems(parityBlock, tryNumber + 1)
             }
         }
+    }
+
+    private fun getExistingAddressesUsedInBlock(
+            block: EthereumBlock,
+            ethereumDaoService: EthereumDaoService = ApplicationContext.ethereumDaoService): List<EthereumAddress> {
+
+        return ethereumDaoService.getAddressesWithLastTransactionBeforeGivenBlock(block.addressesUsedInBlock(), block.number)
     }
 }
---
+++ stream-processing/ethereum/src/main/kotlin/fund/cyber/index/ethereum/converter/AddressConverter.kt
@@ -0,0 +1,107 @@
+package fund.cyber.index.ethereum.converter
+
+import fund.cyber.node.model.*
+import org.slf4j.LoggerFactory
+import java.math.BigDecimal
+
+private val log = LoggerFactory.getLogger(EthereumAddressConverter::class.java)!!
+
+class EthereumAddressConverter {
+
+
+    fun updateAddressesSummary(
+            newBlock: EthereumBlock,
+            existingAddressesUsedInBlock: List<EthereumAddress>): List<EthereumAddress> {
+
+        val addressLookUp = existingAddressesUsedInBlock.associateBy { address -> address.id }.toMutableMap()
+
+        return newBlock.transactions
+                .flatMap { tx ->
+                    listOf(
+                            updateAddressByTransaction(newBlock.number, addressLookUp[tx.from]!!, tx),
+                            updateAddressByInputTransaction(newBlock.number, addressLookUp[tx.to], tx.to, tx)
+                    ).apply { this.forEach { address -> addressLookUp.put(address.id, address) } }
+                }
+    }
+
+
+    fun updateAddressByInputTransaction(blockNumber: Long, address: EthereumAddress?,
+                                        addressId: String, tx: EthereumBlockTransaction): EthereumAddress {
+
+        if (address == null) {
+            log.trace("first address transaction: $addressId")
+            return EthereumAddress(
+                    id = addressId, last_transaction_block = blockNumber, tx_number = 1,
+                    balance = tx.amount.toString(), total_received = tx.amount.toString()
+            )
+        }
+        return updateAddressByTransaction(blockNumber, address, tx)
+    }
+
+
+    fun updateAddressByTransaction(
+            blockNumber: Long, address: EthereumAddress, tx: EthereumBlockTransaction): EthereumAddress {
+
+        val isOutgoingTransaction = tx.from == address.id
+
+        val balance =
+                if (isOutgoingTransaction) BigDecimal(address.balance) - tx.amount
+                else BigDecimal(address.balance) + tx.amount
+
+        val totalReceived =
+                if (isOutgoingTransaction) address.total_received
+                else (BigDecimal(address.total_received) + tx.amount).toString()
+
+        return EthereumAddress(
+                id = address.id, last_transaction_block = blockNumber, tx_number = address.tx_number + 1,
+                balance = balance.toString(), total_received = totalReceived
+        )
+    }
+
+
+    private fun nonExistingAddressFromOutput(
+            blockNumber: Long, addressId: String, output: BitcoinTransactionOut): BitcoinAddress {
+
+        return BitcoinAddress(
+                id = addressId, last_transaction_block = blockNumber, tx_number = 1,
+                total_received = output.amount, balance = output.amount
+        )
+    }
+
+
+    private fun updateAddressByTransactionOutput(
+            blockNumber: Long, address: BitcoinAddress, output: BitcoinTransactionOut): BitcoinAddress {
+
+        return BitcoinAddress(
+                id = address.id, last_transaction_block = blockNumber,
+                tx_number = address.tx_number + 1,
+                total_received = (BigDecimal(address.total_received) + BigDecimal(output.amount)).toString(),
+                balance = (BigDecimal(address.balance) + BigDecimal(output.amount)).toString()
+        )
+    }
+
+
+    private fun updateAddressByTransactionInput(
+            blockNumber: Long, address: BitcoinAddress, input: BitcoinTransactionIn): BitcoinAddress {
+
+        return BitcoinAddress(
+                id = address.id, last_transaction_block = blockNumber,
+                tx_number = address.tx_number + 1, total_received = address.total_received,
+                balance = (BigDecimal(address.balance) - BigDecimal(input.amount)).toString()
+        )
+    }
+
+
+    fun transactionsPreviewsForAddresses(newBlock: EthereumBlock): List<EthereumAddressTransaction> {
+
+        return newBlock.transactions
+                .flatMap { tx ->
+                    tx.addressesUsedInTransaction().map { addressId ->
+                        EthereumAddressTransaction(
+                                address = addressId, fee = tx.fee.toString(), block_time = newBlock.timestamp,
+                                hash = tx.hash, value = tx.amount.toString(), from = tx.from, to = tx.to
+                        )
+                    }
+                }
+    }
+}
