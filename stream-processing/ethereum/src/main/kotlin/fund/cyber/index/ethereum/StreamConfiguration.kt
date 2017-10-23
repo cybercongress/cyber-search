03054b9436f691dd0ae3026b742fc270f0860c59
Hleb Albau <hleb.albau@gmail.com>
2017-10-23 13:52:52 +0300

    #18 Add start block configuration

--- stream-processing/ethereum/src/main/kotlin/fund/cyber/index/ethereum/StreamConfiguration.kt
+++ stream-processing/ethereum/src/main/kotlin/fund/cyber/index/ethereum/StreamConfiguration.kt
@@ -45,6 +45,7 @@ class StreamConfiguration(
         val cassandraServers: String = env("CASSANDRA_CONNECTION", "localhost"),
         private val kafkaServers: String = env("KAFKA_CONNECTION", "localhost:9092"),
         private val stateStateCommitTime: Long = env("COMMIT_STATE_MS", 10),
+        val processLastBlock: Long = env("PROCESS_LAST_BLOCK", -1L),
         private val applicationIdMinorVersion: String = env("APPLICATION_ID_SUFFIX", "0"),
         private val applicationId: String = "cyber.index.ethereum.block.splitter.v1.$applicationIdMinorVersion"
 ) {
