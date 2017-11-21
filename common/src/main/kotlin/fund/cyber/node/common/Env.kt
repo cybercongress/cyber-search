a5f56dfc5174d0cad008763caa797823a34166da
Hleb Albau <hleb.albau@gmail.com>
2017-11-21 13:40:21 +0300

    Add support for elastic migration through http requests

--- common/src/main/kotlin/fund/cyber/node/common/Env.kt
+++ common/src/main/kotlin/fund/cyber/node/common/Env.kt
@@ -6,6 +6,15 @@ const val CASSANDRA_HOSTS_DEFAULT = "localhost"
 const val CASSANDRA_PORT = "CASSANDRA_PORT"
 const val CASSANDRA_PORT_DEFAULT = 9042
 
+const val ELASTIC_HOSTS = "ELASTIC_HOSTS"
+const val ELASTIC_HOSTS_DEFAULT = "localhost"
+
+const val ELASTIC_HTTP_PORT = "ELASTIC_HTTP_PORT"
+const val ELASTIC_HTTP_PORT_DEFAULT = 9200
+
+const val ELASTIC_TRANSPORT_PORT = "ELASTIC_TRANSPORT_PORT"
+const val ELASTIC_TRANSPORT_PORT_DEFAULT = 9300
+
 
 inline fun <reified T : Any> env(name: String, default: T): T =
         when (T::class) {
---
+++ dao-service/src/main/kotlin/fund/cyber/dao/migration/ElassandraSchemaMigrationEngine.kt
@@ -0,0 +1,71 @@
+package fund.cyber.dao.migration
+
+import com.datastax.driver.core.Cluster
+import com.datastax.driver.core.Session
+import fund.cyber.dao.system.SystemDaoService
+import fund.cyber.node.model.SchemaVersion
+import org.apache.http.client.HttpClient
+import org.apache.http.client.methods.RequestBuilder
+import org.slf4j.LoggerFactory
+import java.net.URI
+import java.util.*
+
+class ElassandraSchemaMigrationEngine(
+        elasticHost: String,
+        elasticPort: Int,
+        private val cassandra: Cluster,
+        private val httpClient: HttpClient,
+        private val systemDaoService: SystemDaoService,
+        private val migrations: List<Migration>
+) {
+
+    private val elasticBaseUri = URI.create("http://$elasticHost:$elasticPort")
+    private val log = LoggerFactory.getLogger(ElassandraSchemaMigrationEngine::class.java)
+
+
+    fun executeSchemaUpdate() {
+
+        log.info("Executing elassandra schema update")
+        val session: Session = cassandra.connect()
+
+        migrations.groupBy { m -> m.applicationId }.forEach { applicationId, applicationMigrations ->
+
+            val lastMigrationVersion = systemDaoService.getLastMigration(applicationId)?.version ?: -1
+
+            applicationMigrations
+                    .filter { migration -> migration.version > lastMigrationVersion }
+                    .sortedBy { migration -> migration.version }
+                    .forEach { migration ->
+                        log.info("Executing '$applicationId' application migration to '${migration.version}' version")
+                        executeMigration(migration, session)
+                        log.info("Succeeded '$applicationId' application migration to '${migration.version}' version")
+                    }
+
+        }
+
+        session.closeAsync()
+        log.info("Elassandra schema update done")
+    }
+
+
+    private fun executeMigration(migration: Migration, session: Session) {
+
+        when (migration) {
+            is CassandraMigration -> {
+                migration.getStatements().forEach { statement -> session.execute(statement) }
+            }
+            is ElasticHttpMigration -> {
+                val requestWithRelativeUri = migration.getRequest()
+                val fullUri = elasticBaseUri.resolve(requestWithRelativeUri.uri)
+                val request = RequestBuilder.copy(requestWithRelativeUri).setUri(fullUri).build()
+                httpClient.execute(request)
+            }
+        }
+
+        val schemeMigrationRecord = SchemaVersion(
+                application_id = migration.applicationId, version = migration.version,
+                apply_time = Date(), migration_hash = migration.hashCode()
+        )
+        systemDaoService.schemaVersionMapper.save(schemeMigrationRecord)
+    }
+}
