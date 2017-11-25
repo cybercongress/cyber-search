eb32f04f35f85c32837afeec40b0d1c15f75094a
Hleb Albau <hleb.albau@gmail.com>
2017-11-25 10:45:44 +0300

    Add chains enum, refactor pumps contexts

--- pumps/src/main/kotlin/fund/cyber/pump/PumpsMigrations.kt
+++ pumps/src/main/kotlin/fund/cyber/pump/PumpsMigrations.kt
@@ -4,7 +4,8 @@ import fund.cyber.dao.migration.CqlFileBasedMigration
 
 object PumpsMigrations {
 
-    private val applicationId = "pump.common"
+    const val pumpsApplicationIdPrefix = "PUMP."
+    private const val applicationId = pumpsApplicationIdPrefix + "COMMON"
 
     val migrations = listOf(
             CqlFileBasedMigration(0, applicationId, "/migrations/pump/0_initial.cql")
--- pumps/src/main/kotlin/fund/cyber/pump/ethereum/EthereumMigrations.kt
+++ pumps/src/main/kotlin/fund/cyber/pump/ethereum/EthereumMigrations.kt
@@ -1,18 +1,21 @@
-package fund.cyber.pump.ethereum
+package fund.cyber.pump.applicationId
 
+import Chains.ETHEREUM
 import fund.cyber.dao.migration.CqlFileBasedMigration
 import fund.cyber.dao.migration.ElasticHttpMigration
-import fund.cyber.pump.PumpsApplications.ETHEREUM
+import fund.cyber.pump.PumpsMigrations
 
 object EthereumMigrations {
 
+    val applicationId = PumpsMigrations.pumpsApplicationIdPrefix + ETHEREUM
+
     val migrations = listOf(
-            CqlFileBasedMigration(0, ETHEREUM, "/migrations/ethereum/0_initial.cql"),
-            ElasticHttpMigration(1, ETHEREUM, "/migrations/ethereum/1_create-tx-index.json"),
-            ElasticHttpMigration(2, ETHEREUM, "/migrations/ethereum/2_create-tx-type.json"),
-            ElasticHttpMigration(3, ETHEREUM, "/migrations/ethereum/3_create-block-index.json"),
-            ElasticHttpMigration(4, ETHEREUM, "/migrations/ethereum/4_create-block-type.json"),
-            ElasticHttpMigration(5, ETHEREUM, "/migrations/ethereum/5_create-address-index.json"),
-            ElasticHttpMigration(6, ETHEREUM, "/migrations/ethereum/6_create-address-type.json")
+            CqlFileBasedMigration(0, applicationId, "/migrations/applicationId/0_initial.cql"),
+            ElasticHttpMigration(1, applicationId, "/migrations/applicationId/1_create-tx-index.json"),
+            ElasticHttpMigration(2, applicationId, "/migrations/applicationId/2_create-tx-type.json"),
+            ElasticHttpMigration(3, applicationId, "/migrations/applicationId/3_create-block-index.json"),
+            ElasticHttpMigration(4, applicationId, "/migrations/applicationId/4_create-block-type.json"),
+            ElasticHttpMigration(5, applicationId, "/migrations/applicationId/5_create-address-index.json"),
+            ElasticHttpMigration(6, applicationId, "/migrations/applicationId/6_create-address-type.json")
     )
 }
