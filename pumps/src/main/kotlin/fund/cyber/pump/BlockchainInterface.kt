52137ce5e66cbf93ae8724f5617f13f6b219247e
Mikhail Ioda <mikhailiodo@gmail.com>
2017-11-27 17:31:51 +0300

    Add BlockBundle interface

--- pumps/src/main/kotlin/fund/cyber/pump/BlockchainInterface.kt
+++ pumps/src/main/kotlin/fund/cyber/pump/BlockchainInterface.kt
@@ -3,8 +3,13 @@ package fund.cyber.pump
 import fund.cyber.node.common.Chain
 import io.reactivex.Flowable
 
-
-interface BlockchainInterface<T> {
+interface BlockchainInterface<T: BlockBundle> {
     val chain: Chain
     fun subscribeBlocks(startBlockNumber: Long): Flowable<T>
+}
+
+interface BlockBundle {
+    val hash: String
+    val parentHash: String
+    val number: Long
 }
