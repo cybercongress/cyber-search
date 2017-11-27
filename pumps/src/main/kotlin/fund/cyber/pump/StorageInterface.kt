d069f4e9fa49406932ff23c488ca8e967aa405ba
Mikhail Ioda <mikhailiodo@gmail.com>
2017-11-27 19:42:12 +0300

    Nonnull constructAction

--- pumps/src/main/kotlin/fund/cyber/pump/StorageInterface.kt
+++ pumps/src/main/kotlin/fund/cyber/pump/StorageInterface.kt
@@ -2,10 +2,24 @@ package fund.cyber.pump
 
 interface StorageInterface {
     fun initialize(blockchainInterfaces: List<BlockchainInterface<*>>)
-    fun constructAction(blockBundle: BlockBundle): StorageAction?
+    fun constructAction(blockBundle: BlockBundle): StorageAction
 }
 
 interface StorageAction {
+    companion object {
+        val empty = EmptyStorageAction
+    }
+
     fun store()
     fun remove()
+}
+
+object EmptyStorageAction: StorageAction {
+    override fun store() {
+        println("StorageAction: empty.store()")
+    }
+
+    override fun remove() {
+        println("StorageAction: empty.remove()")
+    }
 }
