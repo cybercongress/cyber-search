9e171bc40aa3a51faaa38a014241743d7d27ae1e
Hleb Albau <hleb.albau@gmail.com>
2017-11-30 11:30:52 +0300

    Update project libs

--- common/src/main/kotlin/fund/cyber/node/common/Env.kt
+++ common/src/main/kotlin/fund/cyber/node/common/Env.kt
@@ -1,5 +1,8 @@
 package fund.cyber.node.common
 
+const val KAFKA_BROKERS = "CASSANDRA_HOSTS"
+const val KAFKA_BROKERS_DEFAULT = "localhost:9092"
+
 const val CASSANDRA_HOSTS = "CASSANDRA_HOSTS"
 const val CASSANDRA_HOSTS_DEFAULT = "localhost"
 
@@ -14,6 +17,9 @@ const val ELASTIC_HTTP_PORT_DEFAULT = 9200
 
 const val ELASTIC_TRANSPORT_PORT = "ELASTIC_TRANSPORT_PORT"
 const val ELASTIC_TRANSPORT_PORT_DEFAULT = 9300
+
+const val ELASTIC_CLUSTER_NAME = "ELASTIC_CLUSTER_NAME"
+const val ELASTIC_CLUSTER_NAME_DEFAULT = "CYBER_SEARCH"
 
 
 inline fun <reified T : Any> env(name: String, default: T): T =
