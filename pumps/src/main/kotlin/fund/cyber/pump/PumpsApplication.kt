5e1a3bbaeb9da7d62aad8269bd1e3d402dfa3feb
Hleb Albau <hleb.albau@gmail.com>
2017-12-06 19:28:42 +0300

    Add correct exit on flowable error

--- pumps/src/main/kotlin/fund/cyber/pump/PumpsApplication.kt
+++ pumps/src/main/kotlin/fund/cyber/pump/PumpsApplication.kt
@@ -12,47 +12,43 @@ import fund.cyber.pump.ethereum_classic.EthereumClassicBlockchainInterface
 
 object PumpsApplication {
 
+    private val storages: List<StorageInterface> = listOf(PumpsContext.elassandraStorage)
+
     @JvmStatic
     fun main(args: Array<String>) {
-        val storages: List<StorageInterface> = listOf(PumpsContext.elassandraStorage)
-        PumpsConfiguration.chainsToPump.forEach { chain -> startChainPumper(chain, storages) }
+        PumpsConfiguration.chainsToPump.forEach { chain -> startChainPumper(chain) }
     }
-}
 
 
-private fun startChainPumper(chain: Chain, storages: List<StorageInterface>) {
-    when (chain) {
-        BITCOIN -> {
-            val flowableInterface = ConcurrentPulledBlockchain(BitcoinBlockchainInterface())
-            ChainPumper(
-                    blockchainInterface = flowableInterface,
-                    storageActionsFactories = listOf(BitcoinCassandraActionFactory()),
-                    storages = storages, stateStorage = PumpsContext.elassandraStorage
-            ).start()
+    private fun startChainPumper(chain: Chain) {
+        when (chain) {
+            BITCOIN -> {
+                val flowableInterface = ConcurrentPulledBlockchain(BitcoinBlockchainInterface())
+                getChainPumper(flowableInterface, listOf(BitcoinCassandraActionFactory())).start()
+            }
+            BITCOIN_CASH -> {
+                val flowableInterface = ConcurrentPulledBlockchain(BitcoinCashBlockchainInterface())
+                getChainPumper(flowableInterface, listOf(BitcoinCassandraActionFactory())).start()
+            }
+            ETHEREUM -> {
+                val flowableInterface = ConcurrentPulledBlockchain(EthereumBlockchainInterface())
+                getChainPumper(flowableInterface, listOf(EthereumCassandraActionFactory())).start()
+            }
+            ETHEREUM_CLASSIC -> {
+                val flowableInterface = ConcurrentPulledBlockchain(EthereumClassicBlockchainInterface())
+                getChainPumper(flowableInterface, listOf(EthereumCassandraActionFactory())).start()
+            }
         }
-        BITCOIN_CASH -> {
-            val flowableInterface = ConcurrentPulledBlockchain(BitcoinCashBlockchainInterface())
-            ChainPumper(
-                    blockchainInterface = flowableInterface,
-                    storageActionsFactories = listOf(BitcoinCassandraActionFactory()),
-                    storages = storages, stateStorage = PumpsContext.elassandraStorage
-            ).start()
-        }
-        ETHEREUM -> {
-            val flowableInterface = ConcurrentPulledBlockchain(EthereumBlockchainInterface())
-            ChainPumper(
-                    blockchainInterface = flowableInterface,
-                    storageActionsFactories = listOf(EthereumCassandraActionFactory()),
-                    storages = storages, stateStorage = PumpsContext.elassandraStorage
-            ).start()
-        }
-        ETHEREUM_CLASSIC -> {
-            val flowableInterface = ConcurrentPulledBlockchain(EthereumClassicBlockchainInterface())
-            ChainPumper(
-                    blockchainInterface = flowableInterface,
-                    storageActionsFactories = listOf(EthereumCassandraActionFactory()),
-                    storages = storages, stateStorage = PumpsContext.elassandraStorage
-            ).start()
-        }
+    }
+
+
+    private fun <T : BlockBundle> getChainPumper(
+            flowableInterface: FlowableBlockchainInterface<T>, actionFactories: List<StorageActionFactory>
+    ): ChainPumper<T> {
+
+        return ChainPumper(
+                blockchainInterface = flowableInterface, storageActionsFactories = actionFactories,
+                storages = storages, stateStorage = PumpsContext.elassandraStorage
+        )
     }
 }
