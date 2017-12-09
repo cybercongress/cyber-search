0db438842e48a6175e7d7caaf56ed720f3957228
Hleb Albau <hleb.albau@gmail.com>
2017-12-10 00:44:22 +0300

    Fix zombies processes for address service

--- address-service/src/main/kotlin/fund/cyber/address/common/AddressDelta.kt
+++ address-service/src/main/kotlin/fund/cyber/address/common/AddressDelta.kt
@@ -5,7 +5,8 @@ import java.math.BigDecimal
 
 data class AddressDelta(
         val address: String,
-        val delta: BigDecimal
+        val delta: BigDecimal,
+        val blockNumber: Long
 )
 
 
