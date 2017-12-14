a796ed525285a889d0bd10acc0bad95ed0d66929
Hleb Albau <hleb.albau@gmail.com>
2017-12-14 13:01:24 +0300

    Add basic address txes handlers implementation

--- cassandra-service/src/main/kotlin/fund/cyber/cassandra/repository/EthereumKeyspaceRepository.kt
+++ cassandra-service/src/main/kotlin/fund/cyber/cassandra/repository/EthereumKeyspaceRepository.kt
@@ -1,15 +1,28 @@
 package fund.cyber.cassandra.repository
 
 import com.datastax.driver.core.Cluster
+import com.datastax.driver.mapping.Result
+import com.datastax.driver.mapping.annotations.Accessor
+import com.datastax.driver.mapping.annotations.Query
+import com.google.common.util.concurrent.ListenableFuture
 import fund.cyber.cassandra.CassandraKeyspaceRepository
 import fund.cyber.cassandra.model.keyspace
 import fund.cyber.node.common.Chain
 import fund.cyber.node.model.EthereumAddress
+import fund.cyber.node.model.EthereumAddressTxPreview
 
+
+@Accessor
+interface EthereumKeyspaceRepositoryAccessor {
+
+    @Query("SELECT * FROM tx_preview_by_address where address=? limit 20")
+    fun addressTransactions(address: String): ListenableFuture<Result<EthereumAddressTxPreview>>
+}
 
 class EthereumKeyspaceRepository(
         cassandra: Cluster, chain: Chain
 ) : CassandraKeyspaceRepository(cassandra, chain.keyspace) {
 
     val addressStore = mappingManager.mapper(EthereumAddress::class.java)!!
+    val ethereumKeyspaceRepositoryAccessor = mappingManager.createAccessor(EthereumKeyspaceRepositoryAccessor::class.java)!!
 }
---
+++ search-api/src/main/kotlin/fund/cyber/search/handler/EthereumAddressTxHandler.kt
@@ -0,0 +1,37 @@
+package fund.cyber.search.handler
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import fund.cyber.cassandra.repository.EthereumKeyspaceRepository
+import fund.cyber.node.common.stringValue
+import fund.cyber.search.configuration.AppContext
+import io.undertow.server.HttpHandler
+import io.undertow.server.HttpServerExchange
+import io.undertow.util.Headers
+
+
+class EthereumAddressTxHandler(
+        repository: EthereumKeyspaceRepository,
+        private val jsonSerializer: ObjectMapper = AppContext.jsonSerializer
+) : HttpHandler {
+
+
+    private val addressTxTable = repository.ethereumKeyspaceRepositoryAccessor
+
+    override fun handleRequest(exchange: HttpServerExchange) {
+
+        val address = exchange.queryParameters["address"]?.stringValue()
+
+        if (address == null) {
+            exchange.statusCode = 400
+            return
+        }
+
+        val addressTxesFuture = addressTxTable.addressTransactions(address).get()
+
+        val txes = addressTxesFuture?.all() ?: emptyList()
+
+        val rawResponse = jsonSerializer.writeValueAsString(txes)
+        exchange.responseHeaders.put(Headers.CONTENT_TYPE, "application/json")
+        exchange.responseSender.send(rawResponse)
+    }
+}
