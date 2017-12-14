51dd984063896170c7130ca1456501a0d8861c42
Hleb Albau <hleb.albau@gmail.com>
2017-12-14 13:01:24 +0300

    Add basic address txes handlers implementation

--- search-api/src/main/kotlin/fund/cyber/search/SearchApiApplication.kt
+++ search-api/src/main/kotlin/fund/cyber/search/SearchApiApplication.kt
@@ -14,18 +14,36 @@ object SearchApiApplication {
     fun main(args: Array<String>) {
 
         val bitcoinRepository = AppContext.cassandraService.bitcoinRepository
+        val bitcoinCashRepository = AppContext.cassandraService.bitcoinCashRepository
         val ethereumRepository = AppContext.cassandraService.ethereumRepository
+        val ethereumClassicRepository = AppContext.cassandraService.ethereumClassicRepository
 
         val httpHandler = Handlers.routing()
                 .get("/index-stats", IndexStatusHandler(indexToChainEntity = INDEX_TO_CHAIN_ENTITY))
                 .get("/search", SearchHandler(indexToChainEntity = INDEX_TO_CHAIN_ENTITY))
                 .get("/ping", PingHandler())
+
+
                 .get("/bitcoin/block/{blockNumber}", BitcoinBlockHandler(bitcoinRepository))
                 .get("/bitcoin/tx/{txHash}", BitcoinTxHandler(bitcoinRepository))
                 .get("/bitcoin/address/{address}", BitcoinAddressHandler(bitcoinRepository))
+                .get("/bitcoin/address/{address}/transactions", BitcoinAddressTxHandler(bitcoinRepository))
+
+                .get("/bitcoin_cash/block/{blockNumber}", BitcoinBlockHandler(bitcoinCashRepository))
+                .get("/bitcoin_cash/tx/{txHash}", BitcoinTxHandler(bitcoinCashRepository))
+                .get("/bitcoin_cash/address/{address}", BitcoinAddressHandler(bitcoinCashRepository))
+                .get("/bitcoin_cash/address/{address}/transactions", BitcoinAddressTxHandler(bitcoinCashRepository))
+
+
                 .get("/ethereum/block/{blockNumber}", EthereumBlockHandler(ethereumRepository))
                 .get("/ethereum/tx/{txHash}", EthereumTxHandler(ethereumRepository))
                 .get("/ethereum/address/{address}", EthereumAddressHandler(ethereumRepository))
+                .get("/ethereum/address/{address}/transactions", EthereumAddressTxHandler(ethereumRepository))
+
+                .get("/ethereum_classic/block/{blockNumber}", EthereumBlockHandler(ethereumClassicRepository))
+                .get("/ethereum_classic/tx/{txHash}", EthereumTxHandler(ethereumClassicRepository))
+                .get("/ethereum_classic/address/{address}", EthereumAddressHandler(ethereumClassicRepository))
+                .get("/ethereum_classic/address/{address}/transactions", EthereumAddressTxHandler(ethereumClassicRepository))
 
 
         val setCorsHeaderHandler = SetCorsHeadersHandler(httpHandler, SearchApiConfiguration.allowedCORS)
