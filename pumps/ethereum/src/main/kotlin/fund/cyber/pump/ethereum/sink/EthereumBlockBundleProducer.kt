c4e9ecd015fb786faf3648a89232bf5a1eb060b8
Artur Albov <artur.albov@gmail.com>
2018-02-16 20:45:55 +0300

    #52 Refactoring of chainPump. Ethereum chain pump.

--- pumps/ethereum/src/main/kotlin/fund/cyber/pump/ethereum/sink/EthereumBlockBundleProducer.kt
+++ pumps/ethereum/src/main/kotlin/fund/cyber/pump/ethereum/sink/EthereumBlockBundleProducer.kt
@@ -9,14 +9,16 @@ import fund.cyber.search.model.events.txPumpTopic
 import fund.cyber.search.model.events.unclePumpTopic
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
 
 @Component
-class EthereumBlockBundleProducer(
+open class EthereumBlockBundleProducer(
         private val kafkaTemplate: KafkaTemplate<PumpEvent, Any>,
         private val chain: EthereumFamilyChain
 ): KafkaBlockBundleProducer<EthereumBlockBundle> {
 
+    @Transactional
     override fun storeBlockBundle(blockBundle: EthereumBlockBundle) {
         kafkaTemplate.send(chain.blockPumpTopic, PumpEvent.NEW_BLOCK, blockBundle.block)
         blockBundle.transactions.forEach { tx -> kafkaTemplate.send(chain.txPumpTopic, PumpEvent.NEW_BLOCK, tx) }
