72cf130e4247af079618268814193845f5908f04
Artur Albov <artur.albov@gmail.com>
2018-04-09 17:03:55 +0300

    #78 Pumps move kafka producer definition to common

--- pumps/ethereum/src/main/kotlin/fund/cyber/pump/ethereum/client/EthereumClientConfiguration.kt
+++ pumps/ethereum/src/main/kotlin/fund/cyber/pump/ethereum/client/EthereumClientConfiguration.kt
@@ -3,9 +3,14 @@ package fund.cyber.pump.ethereum.client
 import fund.cyber.search.configuration.CHAIN_NODE_URL
 import fund.cyber.search.configuration.ETHEREUM_CHAIN_NODE_DEFAULT_URL
 import fund.cyber.search.configuration.env
+import fund.cyber.search.model.chains.EthereumFamilyChain
+import fund.cyber.search.model.events.blockPumpTopic
+import fund.cyber.search.model.events.txPumpTopic
+import fund.cyber.search.model.events.unclePumpTopic
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager
 import org.apache.http.message.BasicHeader
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.web3j.protocol.Web3j
@@ -16,6 +21,9 @@ const val MAX_TOTAL = 32
 
 @Configuration
 class EthereumClientConfiguration {
+
+    @Autowired
+    private lateinit var chain: EthereumFamilyChain
 
     private val defaultHttpHeaders = listOf(BasicHeader("Keep-Alive", "timeout=10, max=1024"))
     private val connectionManager = PoolingHttpClientConnectionManager().apply {
@@ -34,4 +42,7 @@ class EthereumClientConfiguration {
 
     @Bean
     fun parityClient() = Web3j.build(HttpService(endpointUrl, httpClient()))!!
+
+    @Bean
+    fun kafkaTopicNames(): List<String> = listOf(chain.txPumpTopic, chain.blockPumpTopic, chain.unclePumpTopic)
 }
