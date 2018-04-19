2c0dfd4056080093f7b46ed2af7781808be2f810
Artur Albov <artur.albov@gmail.com>
2018-04-19 15:47:28 +0300

    #106 Add filter params for search api

--- search-api/src/main/kotlin/fund/cyber/api/search/SearchController.kt
+++ search-api/src/main/kotlin/fund/cyber/api/search/SearchController.kt
@@ -1,5 +1,6 @@
 package fund.cyber.api.search
 
+import org.elasticsearch.action.support.IndicesOptions
 import org.elasticsearch.client.transport.TransportClient
 import org.elasticsearch.common.unit.Fuzziness
 import org.elasticsearch.index.query.QueryBuilders
@@ -18,13 +19,17 @@ class SearchController(
     fun search(
             @RequestParam query: String,
             @RequestParam(required = false, defaultValue = "0") page: Int,
-            @RequestParam(required = false, defaultValue = "10") pageSize: Int
+            @RequestParam(required = false, defaultValue = "10") pageSize: Int,
+            @RequestParam(required = false, defaultValue = "") chains: Array<String>,
+            @RequestParam(required = false, defaultValue = "") types: Array<String>
     ): Mono<SearchResponse> {
 
         val elasticQuery = QueryBuilders.matchQuery("_all", query)
                 .fuzziness(Fuzziness.ZERO)
 
-        val elasticResponse = elasticClient.prepareSearch()
+        val elasticResponse = elasticClient.prepareSearch(*prepareIndices(chains, types))
+                .setIndicesOptions(IndicesOptions.lenientExpandOpen())
+                .setTypes()
                 .setQuery(elasticQuery)
                 .setFrom(page * pageSize).setSize(pageSize).setExplain(true)
                 .get()
@@ -41,5 +46,13 @@ class SearchController(
                         items = responseItems, searchTime = elasticResponse.tookInMillis
                 )
         )
+    }
+
+    private fun prepareIndices(chains: Array<String>, types: Array<String>): Array<String> {
+        val chainsToFilter = if (chains.isEmpty()) arrayOf("*") else chains
+        val typesToFilter = if (types.isEmpty()) arrayOf("*") else types
+
+        val indicesToFilter = chainsToFilter.flatMap { chain -> typesToFilter.map { type -> "$chain.$type" } }
+        return indicesToFilter.toTypedArray()
     }
 }
