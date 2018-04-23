1f8ce8044f0cf09b523d7a801042742f5feec233
Artur Albov <artur.albov@gmail.com>
2018-04-23 13:17:14 +0300

    Contract summary. Fix findAllByHash function

--- cassandra-service/src/main/kotlin/fund/cyber/cassandra/bitcoin/configuration/BitcoinRepositoryConfiguration.kt
+++ cassandra-service/src/main/kotlin/fund/cyber/cassandra/bitcoin/configuration/BitcoinRepositoryConfiguration.kt
@@ -140,7 +140,8 @@ class BitcoinRepositoriesConfiguration : InitializingBean {
                 .forEach { keyspace ->
 
                     //create sessions
-                    val converter = MappingCassandraConverter(mappingContext(cluster, keyspace.name))
+                    val converter = MappingCassandraConverter(mappingContext(cluster, keyspace.name,
+                        "fund.cyber.cassandra.bitcoin.model"))
                     val session = getKeyspaceSession(cluster, keyspace.name, converter).also { it.afterPropertiesSet() }
                     val reactiveSession = DefaultReactiveSessionFactory(DefaultBridgedReactiveSession(session.`object`))
 
