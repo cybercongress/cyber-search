2c93a91d5ff53797d8a5707339023e64c3f9defa
Artur Albov <artur.albov@gmail.com>
2018-05-18 19:48:27 +0300

    #92 Pump development guide

--- pumps/common/src/main/kotlin/fund/cyber/pump/common/pool/PoolInterface.kt
+++ pumps/common/src/main/kotlin/fund/cyber/pump/common/pool/PoolInterface.kt
@@ -2,6 +2,7 @@ package fund.cyber.pump.common.pool
 
 import fund.cyber.search.model.PoolItem
 
+// todo: this interface should have one method that returns observable/flowable
 interface PoolInterface<out T: PoolItem> {
     fun onNewItem(action: (T)->Unit, onError: (Throwable) -> Unit)
 }
