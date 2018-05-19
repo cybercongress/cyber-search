4ad3f3ecc141c3c69a0710661ad185666b0f768f
Hleb Albau <hleb.albau@gmail.com>
2018-05-19 20:25:25 +0300

    #132 Wrong ETH Contract Balance
    -- fix wrong serialization

--- common-kafka/src/main/kotlin/fund/cyber/common/kafka/Serialization.kt
+++ common-kafka/src/main/kotlin/fund/cyber/common/kafka/Serialization.kt
@@ -5,8 +5,7 @@ import fund.cyber.search.jsonSerializer
 import org.apache.kafka.common.serialization.Deserializer
 import org.apache.kafka.common.serialization.Serializer
 import org.slf4j.LoggerFactory
-import kotlin.jvm.java
-import kotlin.text.toByteArray
+import java.io.IOException
 
 class JsonSerializer<T> : Serializer<T> {
 
@@ -24,11 +23,20 @@ class JsonDeserializer<T>(private val type: Class<T>) : Deserializer<T> {
 
     private val objectMapper = jsonDeserializer
     private val log = LoggerFactory.getLogger(JsonDeserializer::class.java)
+    private var loggedErrorsCounter = 0
 
-    //todo if exception is throwed here there is no log
     override fun deserialize(topic: String, data: ByteArray): T {
-        log.debug("topic $topic data size : ${data.size}")
-        return objectMapper.readValue(data, type)
+        try {
+            log.trace("topic $topic data size : ${data.size}")
+            return objectMapper.readValue(data, type)
+        } catch (e: IOException) {
+            // log only first exception to not spam log
+            if (loggedErrorsCounter == 0) {
+                log.error("Exception during deserializing $topic", e)
+                loggedErrorsCounter++
+            }
+            throw e
+        }
     }
 
     override fun configure(configs: MutableMap<String, *>, isKey: Boolean) {}
