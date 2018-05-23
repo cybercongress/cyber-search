0b135573006b692a399ffbdd08088975e54babd2
Hleb Albau <hleb.albau@gmail.com>
2018-05-23 13:59:09 +0300

    #132 Wrong ETH Contract Balance
    -- do not store failed contracts bytecode
    -- fix contract creation error detection
    -- serialize numbers as string

--- common/src/main/kotlin/fund/cyber/search/model/ethereum/TxTrace.kt
+++ common/src/main/kotlin/fund/cyber/search/model/ethereum/TxTrace.kt
@@ -1,5 +1,7 @@
 package fund.cyber.search.model.ethereum
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+
 /**
  * Contains full transaction trace tree.
  */
@@ -7,8 +9,10 @@ data class TxTrace(
     val rootOperationTrace: OperationTrace
 ) {
 
+    @JsonIgnore
     fun isRootOperationFailed() = rootOperationTrace.result is ErroredOperationResult
 
+    @JsonIgnore
     fun getAllOperationsTraces() = allSuboperations(listOf(rootOperationTrace))
 
     private fun allSuboperations(operations: List<OperationTrace>): List<OperationTrace> {
