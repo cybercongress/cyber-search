5a93cf5000e8204c980e953e9fd6ebc7652fb1b5
Hleb Albau <hleb.albau@gmail.com>
2018-05-23 17:42:55 +0300

    #132 Wrong ETH Contract Balance
    -- store numbers as plain string

--- common/src/main/kotlin/fund/cyber/search/Serialization.kt
+++ common/src/main/kotlin/fund/cyber/search/Serialization.kt
@@ -8,11 +8,14 @@ import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 
-val jsonSerializer = ObjectMapper().registerKotlinModule()
-    .registerModule(Jdk8Module())
-    .registerModule(JavaTimeModule())
+val jsonSerializer = ObjectMapper()
+    .enable(JsonGenerator.Feature.WRITE_NUMBERS_AS_STRINGS)
+    .enable(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN)
     .setSerializationInclusion(JsonInclude.Include.NON_NULL)
-    .enable(JsonGenerator.Feature.WRITE_NUMBERS_AS_STRINGS)!!
+    .registerKotlinModule()
+    .registerModule(Jdk8Module())
+    .registerModule(JavaTimeModule())!!
+
 
 val jsonDeserializer = ObjectMapper().registerKotlinModule()
     .registerModule(Jdk8Module())
