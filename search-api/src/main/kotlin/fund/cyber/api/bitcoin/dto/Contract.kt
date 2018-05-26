8b7ecc2c812798607b8c2db4633e604d7b5e371f
Artur Albov <artur.albov@gmail.com>
2018-05-26 16:53:56 +0300

    #173 API to return BTC mempool transactions with BTC contract summary

--- search-api/src/main/kotlin/fund/cyber/api/bitcoin/dto/Contract.kt
+++ search-api/src/main/kotlin/fund/cyber/api/bitcoin/dto/Contract.kt
@@ -1,2 +1,47 @@
 package fund.cyber.api.bitcoin.dto
 
+import fund.cyber.cassandra.bitcoin.model.CqlBitcoinContractSummary
+import fund.cyber.cassandra.bitcoin.model.CqlBitcoinContractTxPreview
+import fund.cyber.cassandra.bitcoin.model.CqlBitcoinTxPreviewIO
+import java.math.BigDecimal
+import java.time.Instant
+
+data class ContractTxIODto(
+    val contracts: List<String>,
+    val amount: BigDecimal
+) {
+
+    constructor(io: CqlBitcoinTxPreviewIO) : this(
+        contracts = io.contracts, amount = io.amount
+    )
+}
+
+data class ContractTxSummaryDto(
+    val fee: BigDecimal,
+    val ins: List<ContractTxIODto>,
+    val outs: List<ContractTxIODto>
+) {
+
+    constructor(contractTx: CqlBitcoinContractTxPreview) : this(
+        fee = contractTx.fee, ins = contractTx.ins.map { i -> ContractTxIODto(i) },
+        outs = contractTx.outs.map { o -> ContractTxIODto(o) }
+    )
+}
+
+data class ContractSummaryDto(
+    val hash: String,
+    val confirmedBalance: String,
+    val confirmedTotalReceived: String,
+    val confirmedTxNumber: Int,
+    val firstActivityDate: Instant,
+    val lastActivityDate: Instant,
+    val unconfirmedTxValues: Map<String, ContractTxSummaryDto> = emptyMap()
+) {
+
+    constructor(contract: CqlBitcoinContractSummary, txes: List<CqlBitcoinContractTxPreview>) : this(
+        hash = contract.hash, confirmedBalance = contract.confirmedBalance,
+        confirmedTotalReceived = contract.confirmedTotalReceived, confirmedTxNumber = contract.confirmedTxNumber,
+        firstActivityDate = contract.firstActivityDate, lastActivityDate = contract.lastActivityDate,
+        unconfirmedTxValues = txes.map { contractTx -> contractTx.hash to ContractTxSummaryDto(contractTx) }.toMap()
+    )
+}
