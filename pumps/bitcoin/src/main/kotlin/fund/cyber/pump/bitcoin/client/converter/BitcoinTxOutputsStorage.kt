f40a907f6824288b6c9911adce48239e26fb8e08
Artur Albov <artur.albov@gmail.com>
2018-05-28 22:12:54 +0300

    #169 Improve bitcoin pump performance. Make concurrency env property

--- pumps/bitcoin/src/main/kotlin/fund/cyber/pump/bitcoin/client/converter/BitcoinTxOutputsStorage.kt
+++ pumps/bitcoin/src/main/kotlin/fund/cyber/pump/bitcoin/client/converter/BitcoinTxOutputsStorage.kt
@@ -1,6 +1,8 @@
 package fund.cyber.pump.bitcoin.client.converter
 
 import fund.cyber.pump.bitcoin.client.BitcoinJsonRpcClient
+import fund.cyber.search.configuration.BTC_TX_DOWNLOAD_MAX_CONCURRENCY
+import fund.cyber.search.configuration.BTC_TX_DOWNLOAD_MAX_CONCURRENCY_DEFAULT
 import fund.cyber.search.model.bitcoin.BitcoinCacheTxOutput
 import fund.cyber.search.model.bitcoin.JsonRpcBitcoinBlock
 import fund.cyber.search.model.bitcoin.JsonRpcBitcoinTransaction
@@ -8,6 +10,7 @@ import fund.cyber.search.model.bitcoin.RegularTransactionInput
 import io.micrometer.core.instrument.MeterRegistry
 import org.ehcache.Cache
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.util.concurrent.atomic.AtomicLong
 
@@ -17,7 +20,9 @@ private val log = LoggerFactory.getLogger(BitcoinTxOutputsStorage::class.java)!!
 class BitcoinTxOutputsStorage(
     private val client: BitcoinJsonRpcClient,
     private val cache: Cache<String, BitcoinCacheTxOutput>? = null,
-    monitoring: MeterRegistry
+    monitoring: MeterRegistry,
+    @Value("\${$BTC_TX_DOWNLOAD_MAX_CONCURRENCY:$BTC_TX_DOWNLOAD_MAX_CONCURRENCY_DEFAULT}")
+    private val txMaxConcurrency: String
 ) {
 
     val linkedOutputsFromCache = monitoring.gauge("linked_outputs_from_cache", AtomicLong(0L))!!
@@ -70,13 +75,14 @@ class BitcoinTxOutputsStorage(
             totalLinkedOutputs.set(linkedOutputs.size.toLong())
             linkedOutputsFromCache.set(outputsFromCache.size.toLong())
 
-            val nonCacheOutputs = client.getTxes(outputsIdsWithoutCacheHit.map { out -> out.first }.toSet())
+            val nonCacheOutputs = client
+                .getTxes(outputsIdsWithoutCacheHit.map { out -> out.first }.toSet(), txMaxConcurrency.toInt())
                 .flatMap { tx -> tx.vout.map { out -> BitcoinCacheTxOutput(tx.txid, out) } }
             return outputsFromCache + nonCacheOutputs
         }
 
         return client
-            .getTxes(linkedOutputs.map { out -> out.first }.toSet())
+            .getTxes(linkedOutputs.map { out -> out.first }.toSet(), txMaxConcurrency.toInt())
             .flatMap { tx -> tx.vout.map { out -> BitcoinCacheTxOutput(tx.txid, out) } }
     }
 
