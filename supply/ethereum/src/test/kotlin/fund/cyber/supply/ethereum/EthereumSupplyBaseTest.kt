483dace307a8592f17438ea23ed0d96aa4eeb518
Artur Albov <artur.albov@gmail.com>
2018-07-04 21:02:05 +0300

    #230 Pump Modules Tests. Add dirties context

--- supply/ethereum/src/test/kotlin/fund/cyber/supply/ethereum/EthereumSupplyBaseTest.kt
+++ supply/ethereum/src/test/kotlin/fund/cyber/supply/ethereum/EthereumSupplyBaseTest.kt
@@ -2,10 +2,11 @@ package fund.cyber.supply.ethereum
 
 import fund.cyber.common.kafka.BaseKafkaIntegrationTestWithStartedKafka
 import fund.cyber.supply.CommonConfiguration
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.TestPropertySource
 
-
+@DirtiesContext
 @TestPropertySource(properties = ["CHAIN_FAMILY:ETHEREUM", "GENESIS_SUPPLY:72009990.50"])
 @ContextConfiguration(classes = [ApplicationConfiguration::class, CommonConfiguration::class])
 abstract class EthereumSupplyBaseTest : BaseKafkaIntegrationTestWithStartedKafka()
