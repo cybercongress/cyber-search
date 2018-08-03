c293f113d408b6076c5e2c59699f0bff01c8cf18
Hleb Albau <hleb.albau@gmail.com>
2018-08-03 15:55:42 +0300

    #249 Wrong order of transactions in
    also #248 Set pageable endpoints pageSize limits

--- search-api/src/main/kotlin/fund/cyber/api/common/Request.kt
+++ search-api/src/main/kotlin/fund/cyber/api/common/Request.kt
@@ -3,21 +3,44 @@ package fund.cyber.api.common
 import org.springframework.data.cassandra.core.query.CassandraPageRequest
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Slice
+import org.springframework.http.HttpStatus.FORBIDDEN
+import org.springframework.web.reactive.function.BodyInserters.fromObject
 import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
-inline fun <reified T> ServerRequest.toPageableFlux(getPage: (Pageable) -> Slice<T>): Flux<T> {
+const val MAX_PAGE_SIZE = 100
+const val MIN_PAGE_SIZE = 1
+val pageSizeValidationErrorMessageBody = fromObject("pageSize parameter should be in range [1,100]")
+
+
+inline fun <reified T> ServerRequest.toPageableResponse(
+    getPage: (Pageable) -> Slice<T>
+): Mono<ServerResponse> {
+
+    return toPageableResponse(getPage = getPage, mapPage = { pageFlux -> pageFlux })
+}
+
+
+inline fun <reified T, reified R> ServerRequest.toPageableResponse(
+    getPage: (Pageable) -> Slice<T>, mapPage: (Flux<T>) -> Flux<R>
+): Mono<ServerResponse> {
 
     val page = this.queryParam("page").orElse("0").toInt()
     val pageSize = this.queryParam("pageSize").orElse("20").toInt()
+
+    if (pageSize > MAX_PAGE_SIZE || pageSize < MIN_PAGE_SIZE) {
+        return ServerResponse.status(FORBIDDEN).body(pageSizeValidationErrorMessageBody)
+    }
 
     var slice = getPage(CassandraPageRequest.first(pageSize))
 
     for (i in 1..page) {
         if (slice.hasNext()) {
             slice = getPage(slice.nextPageable())
-        } else return Flux.empty()
+        } else return ServerResponse.notFound().build()
     }
-
-    return Flux.fromIterable(slice.content)
+    val pageFlux = Flux.fromIterable(slice.content)
+    return mapPage(pageFlux).asServerResponse()
 }
---
+++ search-api/src/test/kotlin/fund/cyber/api/ApiTestData.kt
@@ -0,0 +1,123 @@
+package fund.cyber.api
+
+const val CONTRACT_A_TRANSACTION_JSON = """
+                [
+                  {
+                    "hash": "txm2",
+                    "blockNumber": -1,
+                    "blockHash": "",
+                    "coinbase": "",
+                    "firstSeenTime": 1.496402519001E9,
+                    "size": 0,
+                    "fee": "0",
+                    "totalInput": "0",
+                    "totalOutput": "50",
+                    "ins": [],
+                    "outs": [
+                      {
+                        "contracts": [
+                          "a"
+                        ],
+                        "amount": 50,
+                        "asm": "",
+                        "out": 0,
+                        "requiredSignatures": 1
+                      }
+                    ]
+                  },
+                  {
+                    "hash": "txm",
+                    "blockNumber": -1,
+                    "blockHash": "",
+                    "coinbase": "",
+                    "firstSeenTime": 1.496316119001E9,
+                    "size": 0,
+                    "fee": "0",
+                    "totalInput": "0",
+                    "totalOutput": "50",
+                    "ins": [],
+                    "outs": [
+                      {
+                        "contracts": [
+                          "a"
+                        ],
+                        "amount": 50,
+                        "asm": "",
+                        "out": 0,
+                        "requiredSignatures": 1
+                      }
+                    ]
+                  },
+                  {
+                    "hash": "txb",
+                    "blockNumber": 100,
+                    "blockHash": "b",
+                    "coinbase": "",
+                    "firstSeenTime": 1.491045719001E9,
+                    "size": 0,
+                    "fee": "0",
+                    "totalInput": "0",
+                    "totalOutput": "50",
+                    "ins": [],
+                    "outs": [
+                      {
+                        "contracts": [
+                          "a"
+                        ],
+                        "amount": 50,
+                        "asm": "",
+                        "out": 0,
+                        "requiredSignatures": 1
+                      }
+                    ]
+                  },
+                  {
+                    "hash": "txc",
+                    "blockNumber": 53,
+                    "blockHash": "c",
+                    "coinbase": "",
+                    "firstSeenTime": 1.491045719001E9,
+                    "blockTime": 1.491045719001E9,
+                    "size": 0,
+                    "fee": "0",
+                    "totalInput": "0",
+                    "totalOutput": "50",
+                    "ins": [],
+                    "outs": [
+                      {
+                        "contracts": [
+                          "a"
+                        ],
+                        "amount": 50,
+                        "asm": "",
+                        "out": 0,
+                        "requiredSignatures": 1
+                      }
+                    ]
+                  },
+                  {
+                    "hash": "txa",
+                    "blockNumber": 0,
+                    "blockHash": "a",
+                    "coinbase": "",
+                    "firstSeenTime": 1.491045719001E9,
+                    "blockTime": 1.491045719001E9,
+                    "size": 0,
+                    "fee": "0",
+                    "totalInput": "0",
+                    "totalOutput": "50",
+                    "ins": [],
+                    "outs": [
+                      {
+                        "contracts": [
+                          "a"
+                        ],
+                        "amount": 50,
+                        "asm": "",
+                        "out": 0,
+                        "requiredSignatures": 1
+                      }
+                    ]
+                  }
+                ]
+                """
